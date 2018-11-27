54f35947f274b08f13674b4aae8e707039e215ef
Jan Schraff <js-171413@hs-weingarten.de>
2018-11-27 16:44:29 +0100

    fixed groundplane test

---
+++ Assets/scr.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class scr : MonoBehaviour {
+    private PlaneFinderBehaviour p;
+	// Use this for initialization
+	void Start () {
+        p = GetComponent<PlaneFinderBehaviour>();
+        InvokeRepeating("replace", 2.0f, 1.0f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+       
+    }
+
+    void replace()
+    {
+        Vector2 aPosition = new Vector2(0, 0);
+        p.PerformHitTest(aPosition);
+    }
+}
